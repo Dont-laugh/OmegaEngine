bd720d5af8a6ffb8e49786b04a8cbc4939999d1d
DontLaugh <2387471378@qq.com>
2023-05-24 19:28:38 +0800

    Init repository

---
+++ OmegaEditor/GameProject/ProjectBrowserDialog.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+
+namespace Omega
+{
+    /// <summary>
+    /// Interaction logic for ProjectBrowserDialog.xaml
+    /// </summary>
+    public partial class ProjectBrowserDialog : Window
+    {
+        public ProjectBrowserDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void OnOpenProjectClick(object sender, RoutedEventArgs e)
+        {
+            OpenProjectBtn.IsEnabled = true;
+            CreateProjectBtn.IsChecked = false;
+            BrowserContent.Margin = new Thickness(0);
+        }
+
+        private void OnCreateProjectClick(object sender, RoutedEventArgs e)
+        {
+            CreateProjectBtn.IsChecked = true;
+            OpenProjectBtn.IsChecked = false;
+            BrowserContent.Margin = new Thickness(-800, 0, 0, 0);
+        }
+    }
+}
---
+++ OmegaEditor/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+
+namespace Omega
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += OnMainWindowLoaded;
+        }
+
+        private void OnMainWindowLoaded(object sender, RoutedEventArgs e)
+        {
+            Loaded -= OnMainWindowLoaded;
+
+            var projectBrowser = new ProjectBrowserDialog();
+
+            if (projectBrowser.ShowDialog() == false)
+            {
+                Application.Current.Shutdown();
+            }
+            else
+            {
+                // TODO: Open project
+            }
+        }
+    }
+}
